fb6f9527091a3a0ccc05dfab7cac1d3d2c7b8de1
pancham99 <pancham.047@gmail.com>
2025-05-26 11:38:22 +0530

    add

--- utils/db.js
+++ utils/db.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const db_connect = async () => {
     try {
   
-        if(process.env.mode === 'production'){
-             await mongoose.connect(process.env.db_production_url)
+        if(process.env.MODE === 'production'){
+             await mongoose.connect(process.env.DB_PRODUCTION_URL)
              console.log(' production connect database')
 
             }else{
                 console.log('ok')
-                await mongoose.connect(process.env.db_local_url)
+                await mongoose.connect(process.env.DB_LOCAL_URL)
                 console.log('local connect database')
             }
     } catch (error) {
