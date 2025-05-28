f7da4367858d01085d177ad8c3b73846270dd68a
pancham99 <pancham.047@gmail.com>
2025-05-29 00:34:21 +0530

    add

--- server.js
+++ server.js
@@ -18,7 +18,14 @@ const PORT = process.env.PORT || 5000;
 
 // ─── Middleware ───────────────────────────────────────────
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://bakendtopbrefing.vercel.app",
+    "https://topbrefing-admin.vercel.app"
+  ],
+}));
 
 // CORS setup (Allow specific origins)
 // app.use(cors({
