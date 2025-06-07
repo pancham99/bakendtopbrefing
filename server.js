2e87fdc2d334475f3ee2627b59e286dcba8c614f
pancham99 <pancham.047@gmail.com>
2025-06-08 01:06:51 +0530

    add

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors({
 
 
     "https://www.topbriefing.in/",
-    
+    "https://www.topbriefing.in",
     "http://localhost:5173",
     "http://localhost:3000",
     "http://localhost:3001",
