dedb275008616869b34041223e65aead89c0d08a
pancham99 <pancham.047@gmail.com>
2025-06-09 04:09:15 +0530

    add

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(cors({
     "https://www.topbriefing.in",
     "http://localhost:5173",
     "http://localhost:3000",
-    "http://localhost:3001",
+    "https://bakendtopbrefing.vercel.app/",
     "https://bakendtopbrefing.vercel.app",
     "https://topbrefing-admin.vercel.app",
     "https://newsportal-seven.vercel.app"
