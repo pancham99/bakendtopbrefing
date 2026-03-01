35531fb57d3e84814c6f6fd19652401c80773acb
pancham99 <pancham.047@gmail.com>
2026-03-02 02:46:03 +0530

    save

--- utils/db.js
+++ utils/db.js
@@ -1,56 +1,20 @@
-const mongoose = require("mongoose");
-
-let isConnected = false;
-
+const mongoose = require('mongoose');
 const db_connect = async () => {
-  if (isConnected) {
-    return;
-  }
+    try {
+  
+        if(process.env.MODE === 'production'){
+             await mongoose.connect(process.env.DB_PRODUCTION_URL)
+             console.log(' production connect database')
 
-  try {
-    const DB_URI =
-      process.env.NODE_ENV === "production"
-        ? process.env.DB_PRODUCTION_URL
-        : process.env.DB_LOCAL_URL;
+            }else{
+                console.log('ok')
+                await mongoose.connect(process.env.DB_LOCAL_URL)
+                console.log('local connect database')
+            }
+    } catch (error) {
+        console.log(error)
 
-    if (!DB_URI) {
-      throw new Error("Database URI not found in environment variables");
     }
-
-    const conn = await mongoose.connect(DB_URI, {
-      bufferCommands: false,
-      serverSelectionTimeoutMS: 5000,
-    });
-
-    isConnected = conn.connections[0].readyState === 1;
-
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("Database connection failed:", error.message);
-    throw error;
-  }
-};
+}
 
 module.exports = db_connect;
-
-
-// const mongoose = require('mongoose');
-// const db_connect = async () => {
-//     try {
-  
-//         if(process.env.MODE === 'production'){
-//              await mongoose.connect(process.env.DB_PRODUCTION_URL)
-//              console.log(' production connect database')
-
-//             }else{
-//                 console.log('ok')
-//                 await mongoose.connect(process.env.DB_LOCAL_URL)
-//                 console.log('local connect database')
-//             }
-//     } catch (error) {
-//         console.log(error)
-
-//     }
-// }
-
-// module.exports = db_connect;
