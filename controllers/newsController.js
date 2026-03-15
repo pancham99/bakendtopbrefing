1561fa7f07b511b1f10f777d0f639b4111b9c6ef
pancham99 <pancham.047@gmail.com>
2026-03-15 13:47:27 +0530

    save

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -160,7 +160,7 @@ class newsController {
             const news = await newsModel.find({isBreaking: true, status: "active" })
                 .sort({ createdAt: -1 })
                 .limit(5)
-                .select("title slug image category date")
+                .select("title slug image category date createdAt")
                 .lean();
 
            
@@ -362,7 +362,7 @@ class newsController {
                     .find({ category, status: "active" })
                     .sort({ createdAt: -1 })
                     .limit(5)
-                    .select("title slug image writerName date category")
+                    .select("title slug image writerName date category, createdAt")
                     .lean();
 
                 news[category] = data;
@@ -491,7 +491,7 @@ class newsController {
                 .find({ status: "active" })
                 .sort({ createdAt: -1 })
                 .limit(6)
-                .select("title slug image writerName date category")
+                .select("title slug image writerName date category createdAt")
                 .lean();
 
             return res.status(200).json({ latestNews });
